1419964290f41b799f0944dfee2ca47c6b062b9f
takakabe <takakabe5384@gmail.com>
2017-09-28 17:37:13 +0900

    カメラのシャッターをきる

--- photo.sh
+++ photo.sh
@@ -1,4 +1,5 @@
+#!/bin/bash
+
 raspistill -vf -hf -o /home/pi/raspberry_bot/scripts/wawawa.jpg
-curl -F file=@/home/pi/raspberry_bot/scripts/wawawa.jpg -F channels=C6R8F8SGL -F token=xoxb-248950932854-tiJxmBvFvki6PSAigTV2V4Ji https://slack.com/api/files.upload
-#curl -F file=@/home/pi/raspberry_bot/scripts/wawawa.jpg -F channels=C3XDJAEKY -F token=xoxb-248950932854-tiJxmBvFvki6PSAigTV2V4Ji https://slack.com/api/files.upload
+curl -F file=@/home/pi/raspberry_bot/scripts/wawawa.jpg -F channels=CHANNELID -F token=xoxb-000000000000-XXXXXXXXXXXXXXXXXXXXXXXX https://slack.com/api/files.upload
 
